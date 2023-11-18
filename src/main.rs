51935ae25c82c433b3adc10ec307f94945215e17
christoff-linde <christoff.linde@gmail.com>
2023-11-18 14:24:55 +0200

    feat: add update_sensor endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{f32, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
@@ -16,7 +17,9 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        // `POST / update-sensor` goes to `update_sensor`
+        .route("/update-sensor", post(update_sensor));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -46,6 +49,41 @@ async fn create_user(
     // this will be converted into a JSON response
     // with a status code of `201 Created`
     (StatusCode::CREATED, Json(user))
+}
+
+async fn update_sensor(Json(payload): Json<UpdateSensor>) -> (StatusCode, Json<SensorData>) {
+    let utc: DateTime<Utc> = Utc::now();
+    let sensor_data = DataObj {
+        temperature: payload.temperature,
+        humidity: payload.humidity,
+    };
+    let sensor_response = SensorData {
+        timestamp: utc,
+        sensor_id: payload.sensor_id,
+        data: sensor_data,
+    };
+
+    (StatusCode::CREATED, Json(sensor_response))
+}
+
+#[derive(Deserialize)]
+struct UpdateSensor {
+    sensor_id: String,
+    temperature: f32,
+    humidity: f32,
+}
+
+#[derive(Serialize)]
+struct SensorData {
+    timestamp: DateTime<Utc>,
+    sensor_id: String,
+    data: DataObj,
+}
+
+#[derive(Serialize)]
+struct DataObj {
+    temperature: f32,
+    humidity: f32,
 }
 
 // the input to our `create_user` handler
